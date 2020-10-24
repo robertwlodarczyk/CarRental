92652e7a40dd8974fab8461c4a912696b586fa4f
Robert_Wlodarczyk <robert.michal.wlodarczyk@gmail.com>
2020-10-24 13:04:54 +0200

    update AbstractDao

--- src/main/java/pl/sda/dao/AbstractDao.java
+++ src/main/java/pl/sda/dao/AbstractDao.java
@@ -4,6 +4,8 @@ import org.hibernate.Session;
 import org.hibernate.Transaction;
 import pl.sda.database.SessionProvider;
 
+import java.util.List;
+
 public abstract class AbstractDao<T> {
 
     private final Class<T> clazz;
@@ -11,28 +13,43 @@ public abstract class AbstractDao<T> {
     protected AbstractDao(Class<T> clazz) {
         this.clazz = clazz;
     }
-        public void add(T record) {
-            Session session = SessionProvider.getSession();
-            Transaction transaction = session.beginTransaction();
-            session.save(record);
-            transaction.commit();
-            session.close();
-        }
 
-        public void drop(T record){
-            Session session = SessionProvider.getSession();
-            Transaction transaction = session.beginTransaction();
-            session.delete(record);
-            transaction.commit();
-            session.close();
-        }
-
-        public void update(T record){
-            Session session = SessionProvider.getSession();
-            Transaction transaction = session.beginTransaction();
-            session.update(record);
-            transaction.commit();
-            session.close();
-        }
+    public T findById(int id) {
+        Session session = SessionProvider.getSession();
+        T record = session.find(clazz, id);
+        session.close();
+        return record;
     }
+
+    public List<T> findAll() {
+        Session session = SessionProvider.getSession();
+        List<T> records = session.createQuery("from " + clazz.getCanonicalName(), clazz).list();
+        session.close();
+        return records;
+    }
+
+    public void add(T record) {
+        Session session = SessionProvider.getSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(record);
+        transaction.commit();
+        session.close();
+    }
+
+    public void drop(T record) {
+        Session session = SessionProvider.getSession();
+        Transaction transaction = session.beginTransaction();
+        session.delete(record);
+        transaction.commit();
+        session.close();
+    }
+
+    public void update(T record) {
+        Session session = SessionProvider.getSession();
+        Transaction transaction = session.beginTransaction();
+        session.update(record);
+        transaction.commit();
+        session.close();
+    }
+}
 
