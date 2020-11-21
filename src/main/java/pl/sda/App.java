4c0260035df82852b47ed71a019b4325751c9059
Robert_Wlodarczyk <robert.michal.wlodarczyk@gmail.com>
2020-11-21 13:17:16 +0100

    update

--- src/main/java/pl/sda/App.java
+++ src/main/java/pl/sda/App.java
@@ -1,21 +1,56 @@
 package pl.sda;
 
-import pl.sda.dao.CarDao;
-import pl.sda.dao.CurrentRentalsDao;
-import pl.sda.dao.CustomerDao;
-import pl.sda.model.Car;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 
 public class App {
+    private JButton buttonMsg;
+    private JPanel panelMenu;
+    private JButton zwróćButton;
+    private JButton listaSamochodówButton;
+    private JButton listaKlientówButton;
+    private JButton exitButton;
 
-    public static void main(String[] args) {
+    public App() {
+        buttonMsg.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "Hello World!");
 
-        CarDao carDao = new CarDao();
-        CurrentRentalsDao currentRentalsDao = new CurrentRentalsDao();
-        CustomerDao customerDao = new CustomerDao();
+            }
+        });
+        zwróćButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
 
+            }
+        });
+        listaSamochodówButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
 
+            }
+        });
+        listaKlientówButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
 
+            }
+        });
+    }
 
+    public static void main(String[] args) throws IOException {
+        JFrame frame = new JFrame("MENU");
+        JLabel contentPane = new JLabel(new ImageIcon(ImageIO.read(App.class.getClassLoader().getResourceAsStream("background.png"))));
+        JPanel panelMenu = new App().panelMenu;
+        frame.setContentPane(contentPane);
+        frame.add(panelMenu);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
 
 
     }
